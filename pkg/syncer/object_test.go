f015f649f9b07661f19c87f2922e298dad23e085
Robert Stefan <rstefan1@users.noreply.github.com>
2022-01-13 10:56:00 +0200

    Fix go imports

--- pkg/syncer/object_test.go
+++ pkg/syncer/object_test.go
@@ -28,7 +28,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 
-	. "github.com/presslabs/controller-util/syncer"
+	. "github.com/presslabs/controller-util/pkg/syncer"
 )
 
 var _ = Describe("ObjectSyncer", func() {
